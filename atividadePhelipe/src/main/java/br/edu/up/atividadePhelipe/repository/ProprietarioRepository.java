46d7cfce75715b46db7f3ae2320c8a5b06846920
Ganso <Ganso@192.168.15.4>
2020-04-06 10:07:16 -0300

    Funcionalidade @Query

--- atividadePhelipe/src/main/java/br/edu/up/atividadePhelipe/repository/ProprietarioRepository.java
+++ atividadePhelipe/src/main/java/br/edu/up/atividadePhelipe/repository/ProprietarioRepository.java
@@ -1,9 +1,13 @@
 package br.edu.up.atividadePhelipe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.edu.up.atividadePhelipe.entity.Proprietario;
 
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Integer>{
-
+		
+	@Query("SELECT p FROM Proprietario WHERE p.telefone like %?1")
+	public Proprietario getAllByTelefone(String telefone);
+	
 }
